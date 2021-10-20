fbe9e238bd10b8fb5728bea02c539db45f543ba4
Ruhonomg <Rubencamaron10@gmail.com>
2021-10-20 11:01:00 +0200

    V13

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Slider timerSlider;
+   public float gameTime;
+
+    private bool stopTimer;
+
+    public GameObject portal;
+    public Transform portalTransform;
+
+
+    public int chestCount;
+    public int score;
+    public Text score_Text;
+    public GameObject gameOverBox;
+    public GameObject victoryBox;
+    
     void Start()
     {
-        
+        score_Text.text = "0";
+        stopTimer = false;
+        timerSlider.maxValue = gameTime;
+        timerSlider.value = gameTime;
+
     }
 
-    // Update is called once per frame
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void AddPoint()
+    {
+        score++;
+        if(score == chestCount)
+        {
+            Instantiate(portal, portalTransform);
+        }
+    }
+
+
+    
     void Update()
     {
-        
+        float time = gameTime - Time.time;
+
+        int minutes = Mathf.FloorToInt(time / 60);
+        int seconds = Mathf.FloorToInt(time - minutes * 60f);
+
+        if(time<= 0)
+        {
+            stopTimer = true;
+            gameOverBox.SetActive(true);
+        }
+
+        if(stopTimer == false)
+        {
+            timerSlider.value = time;
+            
+        }
     }
 }
